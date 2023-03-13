a393cbfc376824aeedc3b5ede10a6fe72247975d
ustasnov <ustasnov@gmail.com>
2023-03-13 23:49:39 +0700

    Add logout, current and getExchangeRates

--- public/homePage.js
+++ public/homePage.js
@@ -1,12 +1,53 @@
 "use strict";
 
 const logout = new LogoutButton();
-logout.action = function() {
+const ratesBoard = new RatesBoard();
+
+logout.action = function () {
   ApiConnector.logout((responseBody) => {
     if (responseBody.success) {
+      stopUpdateExchangeRates();
       location.reload();
     } else {
       console.error("Ошибка: ", responseBody.error);
     }
   });
 }
+
+ApiConnector.current((responseBody) => {
+  if (responseBody.success) {
+    ProfileWidget.showProfile(responseBody.data);
+  } else {
+    console.error("Ошибка: ", responseBody.error);
+  }
+});
+
+function getExchangeRates() {
+  ApiConnector.getStocks((responseBody) => {
+    if (responseBody.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(responseBody.data);
+    } else {
+      console.error("Ошибка: ", responseBody.error);
+    }
+  });
+}
+
+let intId = null;
+
+function startUpdateExchangeRates() {
+  if (intId !== null) {
+    return;
+  }
+  getExchangeRates();
+  intId = setInterval(getExchangeRates, 60000);
+}
+
+function stopUpdateExchangeRates() {
+  if (intId !== null) {
+    clearInterval(intId);
+    intId = null;
+  }
+}
+
+startUpdateExchangeRates();
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,19 @@
 "use strict";
 
 const userForm = new UserForm();
-userForm.loginFormCallback = function(data) {
+
+userForm.loginFormCallback = function (data) {
   ApiConnector.login(data, (responseBody) => {
+    if (responseBody.success) {
+      location.reload();
+    } else {
+      console.error("Ошибка: ", responseBody.error);
+    }
+  });
+}
+
+userForm.registerFormCallback = function (data) {
+  ApiConnector.register(data, (responseBody) => {
     if (responseBody.success) {
       location.reload();
     } else {
